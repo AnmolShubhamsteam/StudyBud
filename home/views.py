174fc9a841c0f63969cf58d98fd6e821a7c2a464
Anmol Shubham <anmolshubhamsteam69@gmail.com>
2024-05-02 23:20:21 +0530

    Removed Extras

--- home/views.py
+++ home/views.py
@@ -8,8 +8,6 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm 
 
